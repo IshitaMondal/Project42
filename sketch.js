c1197695c26622c92e3d15b4c98687ac16c0da8a
Ishita Mondal <70803670+IshitaMondal@users.noreply.github.com>
2020-11-14 13:01:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var hr, mn, sc;
+var scAngle, mnAngle, hrAngle;
+
+function setup() {
+  createCanvas(400,400);  
+  angleMode(DEGREES);  
+}
+
+function draw() {
+  background(0);  
+  
+  translate(200, 200);
+  rotate(-90);
+
+  //Calculating time using predefined function from p5.js
+  hr = hour();
+  mn = minute();
+  sc = second();
+      
+  scAngle = map(sc, 0, 60, 0, 360);
+  mnAngle = map(mn, 0, 60, 0, 360);
+  hrAngle = map(hr%12, 0, 12, 0, 360);
+ 
+  //drawing the seconds hand
+  push();
+  rotate(scAngle);
+  //rotate the hand based on angle calculated
+  stroke(255, 0, 0);
+  strokeWeight(7);
+  line(0, 0, 100, 0);
+  pop();
+
+  //drawing the minutes hand
+  push();
+  rotate(mnAngle);
+  //rotate the hand based on angle calculated
+  stroke(11, 246, 23);
+  strokeWeight(7);
+  line(0, 0, 85, 0);
+  pop();
+
+  //drawing the hours hand
+  push();
+  rotate(hrAngle);
+  //rotate the hand based on angle calculated
+  stroke(0, 11, 244);
+  strokeWeight(7);
+  line(0, 0, 65, 0);
+  pop();
+
+  push();
+  stroke(255, 0, 255);
+  point(0,0);
+
+  strokeWeight(10);
+  noFill();
+  
+  stroke(255, 0, 0);
+  arc(0, 0, 300, 300, 0, scAngle);
+
+  stroke(11, 246, 23);
+  arc(0, 0, 280, 280, 0, mnAngle);
+
+  stroke(0, 11, 244);
+  arc(0, 0, 260, 260, 0, hrAngle);
+  pop();
+}
